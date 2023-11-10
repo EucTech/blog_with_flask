9e0179d157805db12032984ad555caa182c1e772
EucTech <euccreative@gmail.com>
2023-11-10 04:25:02 +0100

    Added registeration form for my blog

---
+++ forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    """This is a resigtration form"""
+    username = StringField('Username', validators=
+                           [DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=
+                             [DataRequired(), Length(min=8, max=15)])
+    confirm_password = PasswordField('Confirm password', validators=
+                                     [DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign up')
+
+
+class LoginForm(FlaskForm):
+    """This is a login form"""
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=
+                             [DataRequired(), Length(min=8, max=15)])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
