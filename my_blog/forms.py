a2f771663c948bbe5047277a0a00bb3d9708679e
EucTech <euccreative@gmail.com>
2023-11-11 03:46:00 +0100

    updated

--- my_blog/forms.py
+++ my_blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from my_blog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -15,6 +16,15 @@ class RegistrationForm(FlaskForm):
                                      [DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+             raise ValidationError('The username is taken')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+             raise ValidationError('email already exist')
 
 class LoginForm(FlaskForm):
     """This is a login form"""
