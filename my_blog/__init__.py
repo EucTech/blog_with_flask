374a65164026955effa76f5a5c529daa7084f63a
EucTech <euccreative@gmail.com>
2023-11-11 03:43:31 +0100

    updated with some imported modules

--- my_blog/__init__.py
+++ my_blog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 # This is where I set secret key
@@ -7,5 +9,9 @@ app.config['SECRET_KEY'] = '2feea2720d48bd16f90deb7fc68199e4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # db.init_app(app)
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from my_blog import routes
